28fce9df5cefdb9025fe739d9b684601c03147da
radhiyanada <radhiyaqotrunnada26@gmail.com>
2023-04-09 21:22:24 +0700

    message

---
+++ android/app/src/main/kotlin/com/example/favoritepage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.favoritepage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,124 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'datajam.dart';
+import 'favorite.dart';
+
+
+class ListJam extends StatefulWidget {
+  const ListJam({Key? key}) : super(key: key);
+
+  @override
+  _ListJamState createState() => _ListJamState();
+}
+
+class _ListJamState extends State<ListJam> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Daftar Rekomendasi Jam"),
+          centerTitle: true,
+          backgroundColor: Colors.black,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.favorite_border, color: Colors.red,),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => FavoritePage()));
+              }),
+          ],
+        ),
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(18),
+            height: size.height,
+            width: size.width,
+            child: ListView.builder(
+              itemCount: listJam.length,
+              itemBuilder: (BuildContext context, int index) {
+                final DataJam jam = listJam[index];
+                return InkWell(
+                  child: Card(
+                    elevation: 10, //shadow
+                    child: Container(
+                      height: size.height / 7,
+                      padding: const EdgeInsets.all(12),
+                      child: ListTile(
+                        leading: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                height: size.height / 7,
+                                width: 70,
+                                jam.images,
+                                fit: BoxFit.cover,
+                                errorBuilder: (context, error, stackTrace) {
+                                  return const Icon(
+                                    Icons.person,
+                                    size: 90,
+                                  );
+                                },
+                          )),
+                          title: Text(
+                            jam.toko,
+                            style: const TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                                maxLines: 1,
+                            ),
+                          subtitle: Text(
+                            jam.merk,
+                            style: const TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.w400,
+                            ),
+                            maxLines: 1,
+                          ),
+                          trailing: IconButton(
+                            icon: listJam[index].favorite
+                                    ? Icon(Icons.favorite, color: Colors.red,) 
+                                    : Icon(Icons.favorite_border),
+                            onPressed: () {
+                                      setState(() {
+                                        if (listJam[index].favorite == false) {
+                                          listJam[index].favorite = true;
+                                          ScaffoldMessenger.of(context).showSnackBar(
+                                            const SnackBar(
+                                              content:
+                                                  Text('Ditambahkan Ke Favorite'),
+                                              backgroundColor: Colors.blue,
+                                              duration: Duration(
+                                                seconds: 1,
+                                              ),
+                                            ),
+                                          );
+                                        } else {
+                                          listJam[index].favorite = false;
+                                          ScaffoldMessenger.of(context).showSnackBar(
+                                            const SnackBar(
+                                              content: Text(
+                                                'Dihapus Dari Favorite',
+                                              ),
+                                              backgroundColor: Colors.red,
+                                              duration: Duration(
+                                                seconds: 1,
+                                              ),
+                                            ),
+                                          );
+                                        }
+                                      });
+                                  },
+                                ),
+                          
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ));
+  }
+}
+
